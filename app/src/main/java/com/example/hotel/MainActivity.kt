10a37efa7a4a9369f8e66f0cf163bb63c05f2b71
Davi Ximenes de Aquino <davixaquino@gmail.com>
2020-12-04 19:57:05 -0300

    some changes

--- app/src/main/java/com/example/hotel/MainActivity.kt
+++ app/src/main/java/com/example/hotel/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.hotel
 
 import android.os.Bundle
+import android.os.Handler
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import com.example.hotel.data.Hotel
 import com.example.hotel.fragment.HotelListFragment
 
 class MainActivity : AppCompatActivity(),
-        HotelListFragment.OnHotelClickListener
+        HotelListFragment.OnHotelClickListener,
+        SearchView.OnQueryTextListener,
+        MenuItem.OnActionExpandListener
 {
+    private var lastSearchTerm: String = ""
+    private var searchView: SearchView? = null
+    private val listFragment:HotelListFragment by lazy {
+        supportFragmentManager.findFragmentById(R.id.fragmentList) as HotelListFragment
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         println("@@MAIN ACTIVITY")
@@ -22,12 +33,71 @@ class MainActivity : AppCompatActivity(),
         */
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState?.putString(EXTRA_SEARCH_TERM, lastSearchTerm)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        lastSearchTerm = savedInstanceState?.getString(EXTRA_SEARCH_TERM) ?: ""
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+
     override fun onHotelClick(hotel: Hotel) {
         showDetailsActivity(hotel.id)
     }
     private fun showDetailsActivity(hotelId: Long)
     {
         HotelDetailsActivity.open(this,hotelId)
+    }
+
+    private fun showDetailsFragment(hotelId: Long)
+    {
+        searchView?.setOnQueryTextFocusChangeListener(null)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.hotel, menu)
+
+        val searchItem = menu?.findItem(R.id.action_search)
+        searchItem?.setOnActionExpandListener(this)
+
+        searchView = searchItem?.actionView as SearchView
+        searchView?.queryHint = getString(R.string.hint_search)
+        searchView?.setOnQueryTextListener(this)
+
+        if (lastSearchTerm.isNotEmpty()) {
+            Handler().post {
+                val query = lastSearchTerm
+                searchItem.expandActionView()
+                searchView?.setQuery(query, true)
+                searchView?.clearFocus()
+            }
+        }
+        return true
+    }
+
+    override fun onQueryTextSubmit(query: String?) = true
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        lastSearchTerm = newText ?: ""
+        listFragment.search(lastSearchTerm)
+        return true
+    }
+
+    override fun onMenuItemActionExpand(item: MenuItem?) = true
+
+    override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
+        lastSearchTerm = ""
+        listFragment.cleanSearch()
+        return true
+    }
+    companion object {
+        const val EXTRA_SEARCH_TERM = "lastSearch"
     }
 
 
--- app/src/main/java/com/example/hotel/fragment/HotelListFragment.kt
+++ app/src/main/java/com/example/hotel/fragment/HotelListFragment.kt
@@ -41,7 +41,7 @@ class HotelListFragment : ListFragment(), HotelListView
     override fun onListItemClick(l: ListView, v: View, position: Int, id: Long)
     {
         super.onListItemClick(l, v, position, id)
-        val hotel = l?.getItemAtPosition(position) as Hotel
+        val hotel = l.getItemAtPosition(position) as Hotel
         presenter.showHotelDetails(hotel)
     }
 
@@ -49,4 +49,15 @@ class HotelListFragment : ListFragment(), HotelListView
     {
         fun onHotelClick(hotel: Hotel)
     }
+
+    fun search(text:String)
+    {
+        presenter.searchHotels(text)
+    }
+
+    fun cleanSearch()
+    {
+        presenter.searchHotels("")
+    }
+
 }
